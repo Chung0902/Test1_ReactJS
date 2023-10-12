eea5d94f7330b402757e197a8742773cba1e0c17
Chung0902 <chungnguyen.130902@gmail.com>
2023-10-12 12:27:38 +0700

    up

---
+++ src/components/Login/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReCaptcha from "../ReCaptcha";
+import chibiImage from "../../images/chibi2.png";
+
+const Login = () => {
+  return (
+    <div className="container1">
+      <div className="image">
+        <img src={chibiImage} alt="chibi" />
+      </div>
+      <div className="falther">
+        <div className="child_1">
+          <h1 className="the_h1">Đăng nhập</h1>
+          <p className="the_p">Đăng nhập tài khoản của bạn</p>
+          <ReCaptcha />
+        </div>
+        <div className="child_1 image_1 table1">
+          <img src={chibiImage} alt="chibi" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/ReCaptcha/index.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const ReCaptcha = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+  const [isHuman, setIsHuman] = useState(false);
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  const handleEmailBlur = () => {
+    if (formData.email.trim() === "") {
+      setErrors({ ...errors, email: "Vui lòng nhập email" });
+    } else {
+      setErrors({ ...errors, email: "" });
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    if (formData.password.trim() === "") {
+      setErrors({ ...errors, password: "Vui lòng nhập mật khẩu" });
+    } else {
+      setErrors({ ...errors, password: "" });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.email.trim() === "") {
+      setErrors({ ...errors, email: "Vui lòng nhập email" });
+      return;
+    }
+
+    if (formData.password.trim() === "") {
+      setErrors({ ...errors, password: "Vui lòng nhập mật khẩu" });
+      return;
+    }
+
+    if (recaptchaValue === null) {
+      return;
+    }
+
+    // Xử lý đăng nhập hoặc gửi dữ liệu lên máy chủ
+    // Ở đây, thay vì làm thực tế, chúng ta giả định rằng đăng nhập thành công.
+    setLoginSuccess(true);
+  };
+  useEffect(() => {
+    if (loginSuccess) {
+      console.log("Đăng nhập thành công");
+      console.log("Email:", formData.email);
+      console.log("Password:", formData.password);
+    }
+  }, [loginSuccess, formData.email, formData.password]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+
+    if (value !== null) {
+      setIsHuman(true);
+    } else {
+      setIsHuman(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label className="textip">
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            onBlur={handleEmailBlur}
+          />
+          {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+        </label>
+        <br />
+        <label className="textip1">
+          Mật khẩu:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            onBlur={handlePasswordBlur}
+          />
+          {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
+        </label>
+        <br />
+        <div className="captcha">
+          <ReCAPTCHA
+            sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            onChange={handleRecaptchaChange}
+            hl="vi" // Thêm thuộc tính này để đặt ngôn ngữ thành tiếng Việt
+          />
+        </div>
+
+        <br />
+        <button
+          type="submit"
+          disabled={
+            formData.email.trim() === "" ||
+            formData.password.trim() === "" ||
+            recaptchaValue === null
+          }
+          className="btn btn-primary"
+        >
+          Đăng nhập
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReCaptcha;
