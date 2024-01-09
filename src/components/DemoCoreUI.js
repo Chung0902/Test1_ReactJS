952d044711f45708164a2acbcdd53d4d2b8d1a38
Chung0902 <chungnguyen.130902@gmail.com>
2024-01-09 09:41:24 +0700

    up

--- src/components/DemoCoreUI.js
+++ src/components/DemoCoreUI.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import PopupB from './PopupB';
 import '../styles/Popup.css';
 
+
 const DemoCoreUI = () => {
+  // State để kiểm soát trạng thái mở/đóng Popup
   const [isPopupOpen, setPopupOpen] = useState(false);
+
+  // State để lưu trữ đường dẫn hình ảnh được chọn từ Popup
   const [selectedImage, setSelectedImage] = useState(null);
 
+  // Hàm mở Popup
   const openPopup = () => {
     setPopupOpen(true);
   };
 
+  // Hàm đóng Popup
   const closePopup = () => {
     setPopupOpen(false);
   };
 
+  // Hàm xử lý sự kiện khi hình ảnh được chọn từ Popup
   const handleImageSelection = (image) => {
     setSelectedImage(image);
     closePopup();
@@ -22,8 +29,10 @@ const DemoCoreUI = () => {
   return (
     <div>
       <h1>Page A</h1>
+      {/* Nút để mở Popup */}
       <button className='butonpagea' onClick={openPopup}>Open Popup</button>
 
+      {/* Hiển thị hình ảnh đã chọn từ Popup */}
       {selectedImage && (
         <div>
           <h2>Selected Image:</h2>
@@ -31,6 +40,7 @@ const DemoCoreUI = () => {
         </div>
       )}
 
+      {/* Hiển thị Popup khi isPopupOpen là true */}
       {isPopupOpen && (
         <PopupB
           onClose={closePopup}
